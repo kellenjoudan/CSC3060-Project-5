2ffb3bc0d28d3d3843dc33b6fb8e7dfc45a04dfb
kellenjoudan <kellenjoudan@gmail.com>
2026-05-02 20:40:50 +0700

    filter gradient

--- src/kernel/filter_gradient.cpp
+++ src/kernel/filter_gradient.cpp
@@ -127,98 +127,156 @@ void naive_filter_gradient(float& out, const data_struct& data,
 
 void stu_filter_gradient(float& out, const data_struct& data,
                         std::size_t width, std::size_t height) {
-    
+
     const size_t W = width;
     const size_t H = height;
     constexpr float inv9 = 1.0f / 9.0f;
     constexpr float inv81 = inv9 * inv9;
     constexpr float two = 2.0f;
-    
-    // Get raw pointers directly from original data
-    const float* a_ptr = data.a.data();
-    const float* b_ptr = data.b.data();
-    const float* c_ptr = data.c.data();
-    const float* d_ptr = data.d.data();
-    const float* e_ptr = data.e.data();
-    const float* f_ptr = data.f.data();
-    const float* g_ptr = data.g.data();
-    const float* h_ptr = data.h.data();
-    const float* i_ptr = data.i.data();
-    
+
+    // Use float for better SIMD vectorization
     float total = 0.0f;
-    
+
+    // Get raw pointers once
+    const float* const a_ptr = data.a.data();
+    const float* const b_ptr = data.b.data();
+    const float* const c_ptr = data.c.data();
+    const float* const d_ptr = data.d.data();
+    const float* const e_ptr = data.e.data();
+    const float* const f_ptr = data.f.data();
+    const float* const g_ptr = data.g.data();
+    const float* const h_ptr = data.h.data();
+    const float* const i_ptr = data.i.data();
+
     for (size_t y = 1; y + 1 < H; ++y) {
         const size_t y_minus_1 = (y - 1) * W;
         const size_t y_curr = y * W;
         const size_t y_plus_1 = (y + 1) * W;
+
+        // Precompute all row pointers
+        const float* const a0 = a_ptr + y_minus_1;
+        const float* const a1 = a_ptr + y_curr;
+        const float* const a2 = a_ptr + y_plus_1;
         
-        // Row pointers
-        const float* a0 = a_ptr + y_minus_1;
-        const float* a1 = a_ptr + y_curr;
-        const float* a2 = a_ptr + y_plus_1;
+        const float* const b0 = b_ptr + y_minus_1;
+        const float* const b1 = b_ptr + y_curr;
+        const float* const b2 = b_ptr + y_plus_1;
         
-        const float* b0 = b_ptr + y_minus_1;
-        const float* b1 = b_ptr + y_curr;
-        const float* b2 = b_ptr + y_plus_1;
+        const float* const c0 = c_ptr + y_minus_1;
+        const float* const c1 = c_ptr + y_curr;
+        const float* const c2 = c_ptr + y_plus_1;
         
-        const float* c0 = c_ptr + y_minus_1;
-        const float* c1 = c_ptr + y_curr;
-        const float* c2 = c_ptr + y_plus_1;
+        const float* const d0 = d_ptr + y_minus_1;
+        const float* const d1 = d_ptr + y_curr;
+        const float* const d2 = d_ptr + y_plus_1;
         
-        const float* d0 = d_ptr + y_minus_1;
-        const float* d1 = d_ptr + y_curr;
-        const float* d2 = d_ptr + y_plus_1;
+        const float* const e0 = e_ptr + y_minus_1;
+        const float* const e1 = e_ptr + y_curr;
+        const float* const e2 = e_ptr + y_plus_1;
         
-        const float* e0 = e_ptr + y_minus_1;
-        const float* e1 = e_ptr + y_curr;
-        const float* e2 = e_ptr + y_plus_1;
+        const float* const f0 = f_ptr + y_minus_1;
+        const float* const f1 = f_ptr + y_curr;
+        const float* const f2 = f_ptr + y_plus_1;
         
-        const float* f0 = f_ptr + y_minus_1;
-        const float* f1 = f_ptr + y_curr;
-        const float* f2 = f_ptr + y_plus_1;
+        const float* const g0 = g_ptr + y_minus_1;
+        const float* const g2 = g_ptr + y_plus_1;
         
-        const float* g0 = g_ptr + y_minus_1;
-        const float* g2 = g_ptr + y_plus_1;
+        const float* const h0 = h_ptr + y_minus_1;
+        const float* const h2 = h_ptr + y_plus_1;
         
-        const float* h0 = h_ptr + y_minus_1;
-        const float* h2 = h_ptr + y_plus_1;
+        const float* const i0 = i_ptr + y_minus_1;
+        const float* const i2 = i_ptr + y_plus_1;
+
+        // Process 8 pixels at a time for maximum ILP
+        size_t x = 1;
+        const size_t x_end = W - 1;
         
-        const float* i0 = i_ptr + y_minus_1;
-        const float* i2 = i_ptr + y_plus_1;
-        
-        // Process 4 pixels at a time for better ILP
-        for (size_t x = 1; x + 1 < W; ++x) {
-            // Box filter sums
-            float sum_a = a0[x-1] + a0[x] + a0[x+1] +
-                          a1[x-1] + a1[x] + a1[x+1] +
-                          a2[x-1] + a2[x] + a2[x+1];
+        for (; x + 7 < x_end; x += 8) {
+            // Pixel 1
+            const float s1a = a0[x-1] + a0[x] + a0[x+1] + a1[x-1] + a1[x] + a1[x+1] + a2[x-1] + a2[x] + a2[x+1];
+            const float s1b = b0[x-1] + b0[x] + b0[x+1] + b1[x-1] + b1[x] + b1[x+1] + b2[x-1] + b2[x] + b2[x+1];
+            const float s1c = c0[x-1] + c0[x] + c0[x+1] + c1[x-1] + c1[x] + c1[x+1] + c2[x-1] + c2[x] + c2[x+1];
             
-            float sum_b = b0[x-1] + b0[x] + b0[x+1] +
-                          b1[x-1] + b1[x] + b1[x+1] +
-                          b2[x-1] + b2[x] + b2[x+1];
+            // Pixel 2
+            const float s2a = a0[x] + a0[x+1] + a0[x+2] + a1[x] + a1[x+1] + a1[x+2] + a2[x] + a2[x+1] + a2[x+2];
+            const float s2b = b0[x] + b0[x+1] + b0[x+2] + b1[x] + b1[x+1] + b1[x+2] + b2[x] + b2[x+1] + b2[x+2];
+            const float s2c = c0[x] + c0[x+1] + c0[x+2] + c1[x] + c1[x+1] + c1[x+2] + c2[x] + c2[x+1] + c2[x+2];
             
-            float sum_c = c0[x-1] + c0[x] + c0[x+1] +
-                          c1[x-1] + c1[x] + c1[x+1] +
-                          c2[x-1] + c2[x] + c2[x+1];
+            // Pixel 3
+            const float s3a = a0[x+1] + a0[x+2] + a0[x+3] + a1[x+1] + a1[x+2] + a1[x+3] + a2[x+1] + a2[x+2] + a2[x+3];
+            const float s3b = b0[x+1] + b0[x+2] + b0[x+3] + b1[x+1] + b1[x+2] + b1[x+3] + b2[x+1] + b2[x+2] + b2[x+3];
+            const float s3c = c0[x+1] + c0[x+2] + c0[x+3] + c1[x+1] + c1[x+2] + c1[x+3] + c2[x+1] + c2[x+2] + c2[x+3];
             
-            total += (sum_a * sum_b) * inv81 + sum_c * inv9;
+            // Pixel 4
+            const float s4a = a0[x+2] + a0[x+3] + a0[x+4] + a1[x+2] + a1[x+3] + a1[x+4] + a2[x+2] + a2[x+3] + a2[x+4];
+            const float s4b = b0[x+2] + b0[x+3] + b0[x+4] + b1[x+2] + b1[x+3] + b1[x+4] + b2[x+2] + b2[x+3] + b2[x+4];
+            const float s4c = c0[x+2] + c0[x+3] + c0[x+4] + c1[x+2] + c1[x+3] + c1[x+4] + c2[x+2] + c2[x+3] + c2[x+4];
             
-            // Sobel X gradient
-            float dx_d = (d0[x+1] - d0[x-1]) + two * (d1[x+1] - d1[x-1]) + (d2[x+1] - d2[x-1]);
-            float dx_e = (e0[x+1] - e0[x-1]) + two * (e1[x+1] - e1[x-1]) + (e2[x+1] - e2[x-1]);
-            float dx_f = (f0[x+1] - f0[x-1]) + two * (f1[x+1] - f1[x-1]) + (f2[x+1] - f2[x-1]);
+            // Pixel 5
+            const float s5a = a0[x+3] + a0[x+4] + a0[x+5] + a1[x+3] + a1[x+4] + a1[x+5] + a2[x+3] + a2[x+4] + a2[x+5];
+            const float s5b = b0[x+3] + b0[x+4] + b0[x+5] + b1[x+3] + b1[x+4] + b1[x+5] + b2[x+3] + b2[x+4] + b2[x+5];
+            const float s5c = c0[x+3] + c0[x+4] + c0[x+5] + c1[x+3] + c1[x+4] + c1[x+5] + c2[x+3] + c2[x+4] + c2[x+5];
             
+            // Pixel 6
+            const float s6a = a0[x+4] + a0[x+5] + a0[x+6] + a1[x+4] + a1[x+5] + a1[x+6] + a2[x+4] + a2[x+5] + a2[x+6];
+            const float s6b = b0[x+4] + b0[x+5] + b0[x+6] + b1[x+4] + b1[x+5] + b1[x+6] + b2[x+4] + b2[x+5] + b2[x+6];
+            const float s6c = c0[x+4] + c0[x+5] + c0[x+6] + c1[x+4] + c1[x+5] + c1[x+6] + c2[x+4] + c2[x+5] + c2[x+6];
+            
+            // Pixel 7
+            const float s7a = a0[x+5] + a0[x+6] + a0[x+7] + a1[x+5] + a1[x+6] + a1[x+7] + a2[x+5] + a2[x+6] + a2[x+7];
+            const float s7b = b0[x+5] + b0[x+6] + b0[x+7] + b1[x+5] + b1[x+6] + b1[x+7] + b2[x+5] + b2[x+6] + b2[x+7];
+            const float s7c = c0[x+5] + c0[x+6] + c0[x+7] + c1[x+5] + c1[x+6] + c1[x+7] + c2[x+5] + c2[x+6] + c2[x+7];
+            
+            // Pixel 8
+            const float s8a = a0[x+6] + a0[x+7] + a0[x+8] + a1[x+6] + a1[x+7] + a1[x+8] + a2[x+6] + a2[x+7] + a2[x+8];
+            const float s8b = b0[x+6] + b0[x+7] + b0[x+8] + b1[x+6] + b1[x+7] + b1[x+8] + b2[x+6] + b2[x+7] + b2[x+8];
+            const float s8c = c0[x+6] + c0[x+7] + c0[x+8] + c1[x+6] + c1[x+7] + c1[x+8] + c2[x+6] + c2[x+7] + c2[x+8];
+            
+            // Accumulate box filter results
+            total += (s1a * s1b) * inv81 + s1c * inv9;
+            total += (s2a * s2b) * inv81 + s2c * inv9;
+            total += (s3a * s3b) * inv81 + s3c * inv9;
+            total += (s4a * s4b) * inv81 + s4c * inv9;
+            total += (s5a * s5b) * inv81 + s5c * inv9;
+            total += (s6a * s6b) * inv81 + s6c * inv9;
+            total += (s7a * s7b) * inv81 + s7c * inv9;
+            total += (s8a * s8b) * inv81 + s8c * inv9;
+            
+            // Sobel X gradients for 8 pixels
+            for (int k = 0; k < 8; ++k) {
+                size_t xk = x + k;
+                float dx_d = -d0[xk-1] + d0[xk+1] - two*d1[xk-1] + two*d1[xk+1] - d2[xk-1] + d2[xk+1];
+                float dx_e = -e0[xk-1] + e0[xk+1] - two*e1[xk-1] + two*e1[xk+1] - e2[xk-1] + e2[xk+1];
+                float dx_f = -f0[xk-1] + f0[xk+1] - two*f1[xk-1] + two*f1[xk+1] - f2[xk-1] + f2[xk+1];
+                total += dx_d * dx_e + dx_f;
+                
+                // Sobel Y gradients for 8 pixels
+                float dy_g = -g0[xk-1] - two*g0[xk] - g0[xk+1] + g2[xk-1] + two*g2[xk] + g2[xk+1];
+                float dy_h = -h0[xk-1] - two*h0[xk] - h0[xk+1] + h2[xk-1] + two*h2[xk] + h2[xk+1];
+                float dy_i = -i0[xk-1] - two*i0[xk] - i0[xk+1] + i2[xk-1] + two*i2[xk] + i2[xk+1];
+                total += dy_g * dy_h + dy_i;
+            }
+        }
+        
+        // Handle remaining pixels (0-7)
+        for (; x + 1 < x_end; ++x) {
+            const float sum_a = a0[x-1] + a0[x] + a0[x+1] + a1[x-1] + a1[x] + a1[x+1] + a2[x-1] + a2[x] + a2[x+1];
+            const float sum_b = b0[x-1] + b0[x] + b0[x+1] + b1[x-1] + b1[x] + b1[x+1] + b2[x-1] + b2[x] + b2[x+1];
+            const float sum_c = c0[x-1] + c0[x] + c0[x+1] + c1[x-1] + c1[x] + c1[x+1] + c2[x-1] + c2[x] + c2[x+1];
+            total += sum_a * sum_b * inv81 + sum_c * inv9;
+            
+            const float dx_d = -d0[x-1] + d0[x+1] - two*d1[x-1] + two*d1[x+1] - d2[x-1] + d2[x+1];
+            const float dx_e = -e0[x-1] + e0[x+1] - two*e1[x-1] + two*e1[x+1] - e2[x-1] + e2[x+1];
+            const float dx_f = -f0[x-1] + f0[x+1] - two*f1[x-1] + two*f1[x+1] - f2[x-1] + f2[x+1];
             total += dx_d * dx_e + dx_f;
             
-            // Sobel Y gradient
-            float dy_g = (g2[x-1] - g0[x-1]) + two * (g2[x] - g0[x]) + (g2[x+1] - g0[x+1]);
-            float dy_h = (h2[x-1] - h0[x-1]) + two * (h2[x] - h0[x]) + (h2[x+1] - h0[x+1]);
-            float dy_i = (i2[x-1] - i0[x-1]) + two * (i2[x] - i0[x]) + (i2[x+1] - i0[x+1]);
-            
+            const float dy_g = -g0[x-1] - two*g0[x] - g0[x+1] + g2[x-1] + two*g2[x] + g2[x+1];
+            const float dy_h = -h0[x-1] - two*h0[x] - h0[x+1] + h2[x-1] + two*h2[x] + h2[x+1];
+            const float dy_i = -i0[x-1] - two*i0[x] - i0[x+1] + i2[x-1] + two*i2[x] + i2[x+1];
             total += dy_g * dy_h + dy_i;
         }
     }
-    
+
     out = total;
 }
 
